11043c29276d38b485387ca43eb5d837efd13bb7
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-02 14:21:35 +0100

    .

--- embed-file/embed-emulate/main.cpp
+++ embed-file/embed-emulate/main.cpp
@@ -4,8 +4,17 @@ const unsigned char file[] = {
   #embed "embed_example.txt"
 };
 
+// WIP: Attempt on creating a #embed wrapper
+#define INCBIN(FILENAME) \
+  const unsigned char data_##FILENAME[] = { \
+  };
+
+INCBIN(embed_example_txt)
+
 
 int main() {
+    const unsigned char* ptr = data_embed_example_txt;
+ 
     printf("Size   : %lu\n", sizeof(file));
     printf("Content: %.*s\n", (int)sizeof(file), file); // specify size since file content is not null-terminated
     return 0;
