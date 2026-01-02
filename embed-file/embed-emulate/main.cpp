0b044be3c94620afa17a44052b7d87b155873e55
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-01-02 14:08:25 +0100

    fix warning

--- embed-file/embed-emulate/main.cpp
+++ embed-file/embed-emulate/main.cpp
@@ -6,7 +6,7 @@ const unsigned char file[] = {
 
 
 int main() {
-    printf("Size   : %u\n", sizeof(file));
+    printf("Size   : %lu\n", sizeof(file));
     printf("Content: %.*s\n", (int)sizeof(file), file); // specify size since file content is not null-terminated
     return 0;
 }
