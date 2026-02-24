a5fccd00b9d284e383deef77b40590ce0fe13b3b
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-24 16:23:09 +0100

    .

--- images/Fedora-43.bat
+++ images/Fedora-43.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo Building Fedora 43 image...
-::docker build -f Fedora-43.Dockerfile --tag=forderud/unixsharedlibs:43 .
+::docker build -f Fedora-43.Dockerfile --tag=unixsharedlibs:fedora .
 
 echo Building sample project...
-docker run --rm -v %cd%:/host forderud/unixsharedlibs:43 /host/bundle-libstdc++/build_app.sh || exit /b 1
+docker run --rm -v %cd%:/host unixsharedlibs:fedora /host/bundle-libstdc++/build_app.sh || exit /b 1
 
 :: Open an interactive bash shell inside the container
-::docker run --rm -it forderud/unixsharedlibs:43 sh
+::docker run --rm -it unixsharedlibs:fedora sh
