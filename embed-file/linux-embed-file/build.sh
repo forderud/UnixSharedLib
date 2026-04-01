9b608420c489d2f4247a7079c1a879ead3451d33
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-01 18:48:53 +0200

    .

--- embed-file/linux-embed-file/build.sh
+++ embed-file/linux-embed-file/build.sh
@@ -4,7 +4,8 @@ rm -f *.o *.so mainApp parse_elf
 # convert embed_example.txt into an object file to binary embedding
 # -z start-stop-visibility=default (default, internal, hidden, or protected)
 #ld -r -b binary embed_example.txt -z start-stop-visibility=default -z noexecstack -o embed_example.o
-objcopy --input-target binary --output elf64-x86-64 --add-section .note.GNU-stack=/dev/null embed_example.txt embed_example.o
+#objcopy --input-target binary --output elf64-x86-64 --add-section .note.GNU-stack=/dev/null embed_example.txt embed_example.o
+objcopy --input-target binary --output elf64-littleaarch64 --add-section .note.GNU-stack=/dev/null embed_example.txt embed_example.o
 
 echo ""
 echo Building libmylib.so...
