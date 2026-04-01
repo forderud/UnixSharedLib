1dc6cd2ec0ab0c3f8dbe735e893339826ed684fc
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-01 10:11:29 +0200

    .

--- embed-file/linux-embed-file/main.cpp
+++ embed-file/linux-embed-file/main.cpp
@@ -6,7 +6,7 @@ extern char _binary_embed_example_txt_end[];
 
 int main() {
     size_t fileSize = _binary_embed_example_txt_end - _binary_embed_example_txt_start;
-    printf("File size: %zd\n", fileSize);
+    printf("File size   : %zd\n", fileSize);
     printf("File content: ");
     printf("%.*s\n", (int)fileSize, _binary_embed_example_txt_start); // specify size since file content may not be null-terminated
     return 0;
