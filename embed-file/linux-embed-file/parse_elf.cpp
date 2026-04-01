805e134f45da043e24918cd58e48d60ef81ac56f
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-01 12:27:26 +0200

    .

--- embed-file/linux-embed-file/parse_elf.cpp
+++ embed-file/linux-embed-file/parse_elf.cpp
@@ -187,7 +187,7 @@ int main(int argc, char **argv) {
     switch (shdr.sh_type) {
       case SHT_SYMTAB:
         {
-          printf("found symtab table at offset %zd, sh_link %u (index %d)\n", shdr.sh_offset, shdr.sh_link, i);
+          printf("found symtab table, sh_link %u (index %d)\n", shdr.sh_link, i);
           // get corresponding string table entry
           ElfSectionHeader st_shdr(file.ptr(), shdr.sh_link);
           // print symbols
@@ -195,7 +195,7 @@ int main(int argc, char **argv) {
         }
         break;
       case SHT_STRTAB:
-        printf("found string table at offset %zd (index %d)\n", shdr.sh_offset, i);
+        printf("found string table (index %d)\n", i);
         // TODO: have to handle multiple string tables better
         //if (!dynstr_off) {
         //  dynstr_off = shdr.sh_offset;
@@ -204,19 +204,18 @@ int main(int argc, char **argv) {
       case SHT_DYNSYM:
         //dynsym_off = shdr.sh_offset;
         //dynsym_sz = shdr.sh_size;
-        //printf("found dynsym table at offset %zd, size %zd (index %d)\n", shdr.sh_offset, shdr.sh_size, i);
+        //printf("found dynsym table, size %zd (index %d)\n", shdr.sh_size, i);
         break;
       case SHT_PROGBITS:
-        printf("found PROGBITS table at offset %zd, size %zd (index %d)\n", shdr.sh_offset, shdr.sh_size, i);
+        printf("found PROGBITS table, size %zd (index %d)\n", shdr.sh_size, i);
         break;
       default:
-        //printf("found %d table at offset %zd, size %zd (index %d)\n", shdr.sh_type, shdr.sh_offset, shdr.sh_size, i);
+        //printf("found %d table, size %zd (index %d)\n", shdr.sh_type, shdr.sh_size, i);
         break;
     }
 
-    if (shdr.sh_name) {
-      printf("shdr.sh_name=%u (index %d)\n", shdr.sh_name, i);
-    }
+    printf("  sh_offset %zd\n", shdr.sh_offset);
+    printf("  sh_name %u (index %d)\n", shdr.sh_name, i);
   }
 
   printf("\n");
