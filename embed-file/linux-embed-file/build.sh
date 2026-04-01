98e419200463040afaecb07fb21984be55eeaccd
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-01 12:53:05 +0200

    fix linker warning

--- embed-file/linux-embed-file/build.sh
+++ embed-file/linux-embed-file/build.sh
@@ -2,7 +2,7 @@ echo Cleaning up...
 rm -f *.o mainApp
 
 # convert embed_example.txt into an object file to binary embedding
-ld -r -b binary embed_example.txt -o embed_example.o 
+ld -r -b binary embed_example.txt -z noexecstack -o embed_example.o 
 
 echo Building mainApp...
 g++ main.cpp embed_example.o -o mainApp
