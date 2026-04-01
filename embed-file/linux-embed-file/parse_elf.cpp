6e91d58682fd9195f18c57d3ed1f5403e6af08e8
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-01 12:15:20 +0200

    .

--- embed-file/linux-embed-file/parse_elf.cpp
+++ embed-file/linux-embed-file/parse_elf.cpp
@@ -84,9 +84,8 @@ struct ElfSectionHeader : Elf64_Shdr {
     ElfSectionHeader(const char* file_ptr, uint16_t idx) {
         Elf64_Ehdr elf_hdr{};
         memcpy(&elf_hdr, file_ptr, sizeof(elf_hdr));
-
-        size_t offset = elf_hdr.e_shoff + idx*elf_hdr.e_shentsize;
-        memcpy(this, file_ptr + offset, sizeof(Elf64_Shdr));
+        // populate struct
+        memcpy(this, file_ptr + elf_hdr.e_shoff + idx*elf_hdr.e_shentsize, sizeof(Elf64_Shdr));
     }
 };
 static_assert(sizeof(ElfSectionHeader) == sizeof(Elf64_Shdr), "ElfSectionHeader size mismatch");
