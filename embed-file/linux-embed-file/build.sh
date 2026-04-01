abe8a3faba96ee1a299839a246183232d388ac6b
Fredrik Orderud <forderud@gmail.com>
2026-04-01 23:47:26 +0200

    remove redundant "objcopy --input-target binary --output $bfdname --add-section .note.GNU-stack=/dev/null embed_example.txt embed_example.o" approach for binary file embedding.

--- embed-file/linux-embed-file/build.sh
+++ embed-file/linux-embed-file/build.sh
@@ -2,14 +2,7 @@ echo Cleaning up...
 rm -f *.o *.so mainApp parse_elf
 
 # convert embed_example.txt into an object file to binary embedding
-# -z start-stop-visibility=default (default, internal, hidden, or protected)
 ld -r -b binary embed_example.txt -z noexecstack -o embed_example.o
-if [[ $(uname -m) == "aarch64" ]]; then
-    bfdname="elf64-littleaarch64"
-else
-    bfdname="elf64-x86-64"
-fi
-#objcopy --input-target binary --output $bfdname --add-section .note.GNU-stack=/dev/null embed_example.txt embed_example.o
 
 echo ""
 echo Building libmylib.so...
