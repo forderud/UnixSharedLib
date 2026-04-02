1b846188acd93fa47a5889f7e1de44fad3f1b040
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-02 22:19:21 +0200

    .

--- embed-file/mac-embed-file/parse_macho.cpp
+++ embed-file/mac-embed-file/parse_macho.cpp
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <stdexcept>
+#include <unistd.h> // open/close
+#include <sys/mman.h> // mmap/munmap
 #include <mach-o/loader.h>
 #include <mach-o/swap.h>
+#include <fcntl.h> // open/close
+#include <sys/stat.h> // fstat
 
+/** Memory-map a file into the process address space. */
+class FileMap {
+public:
+    FileMap(const char* filename) {
+        int fd = open(filename, O_RDONLY);
+        if (fd == -1) {
+            throw std::runtime_error("file open failed");
+        }
+
+        struct stat sb{};
+        if (fstat(fd, &sb) == -1) { // obtain file size
+            close(fd);
+            throw std::runtime_error("unable to determine file size");
+        }
+        m_size = sb.st_size;
+
+        m_ptr = (const char*)mmap(nullptr, m_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        if (m_ptr == nullptr) {
+            close(fd);
+            throw std::runtime_error("unable to map file");
+        }
+        close(fd);
+    }
+    ~FileMap() {
+        munmap((void*)m_ptr, m_size);
+    }
+    
+    size_t size() const {
+        return m_size;
+    }
+    const char* ptr() const {
+        return m_ptr;
+    }
+    
+private:
+    size_t      m_size = 0;
+    const char* m_ptr = nullptr;
+};
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -10,9 +53,9 @@ int main(int argc, char **argv) {
   }
 
   printf("Parsing Mach-O symbols in %s...\n", argv[1]);
-#if 0
   FileMap file(argv[1]);
 
+#if 0
   Elf64_Ehdr elf_hdr{};
   memcpy(&elf_hdr, file.ptr(), sizeof(elf_hdr));
 
