4ddffe909ac97d823fc6936ee48f480881841a01
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-03 17:17:59 +0200

    follow-up

--- embed-file/mac-embed-file/mylib.cpp
+++ embed-file/mac-embed-file/mylib.cpp
@@ -6,10 +6,15 @@
 
 void print_embedded_file (const char* section_name) {
 #if 0
+    // get image header from a global address
+    Dl_info img_info = {};
+    int img_index = dladdr(&rcs_addr_handle, &img_info);
+    auto* header = (mach_header_64*)img_info.dli_fbase; // or &_mh_execute_header for an executable
+
     // access embedded binary file
     const char segment_name[] = "__TEXT"; // __TEXT for read-only data and __DATA for writable data
     unsigned long embed_example_size = 0;
-    const uint8_t* embed_example_start = getsectiondata(&_mh_execute_header, segment_name, section_name, &embed_example_size);
+    const uint8_t* embed_example_start = getsectiondata(header, segment_name, section_name, &embed_example_size);
 
     printf("Content of %s section (%u bytes):\n", section_name, (unsigned int)embed_example_size);
     printf("%.*s\n", (int)embed_example_size, embed_example_start); // specify size since file content is not null-terminated
