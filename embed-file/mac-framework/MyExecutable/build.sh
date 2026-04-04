f4d2c836dc78a6124c0831debd2165ec3c420377
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-04 23:15:17 +0200

    .

--- embed-file/mac-framework/MyExecutable/build.sh
+++ embed-file/mac-framework/MyExecutable/build.sh
@@ -1,5 +1,7 @@
 echo Cleaning up...
-#rm -r build
+rm MyExecutable
+
 
 echo Building MyExecutable independently...
-gcc -F/Users/forderud/VBoxShare/UnixSharedLib/embed-file/mac-framework/build/MyDynFramework -framework MyDynFramework -o MyExecutable Main.cpp -v
+# add -v to debug include problems
+gcc -F/Users/forderud/VBoxShare/UnixSharedLib/embed-file/mac-framework/build/MyDynFramework -framework MyDynFramework -o MyExecutable Main.cpp
