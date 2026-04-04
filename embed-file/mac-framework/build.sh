43c24cec4b594809b7ee46eb8ba0424e66025819
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-04 17:51:37 +0200

    out-of-source build

--- embed-file/mac-framework/build.sh
+++ embed-file/mac-framework/build.sh
@@ -1,8 +1,8 @@
 echo Cleaning up...
-rm -f *.o *.a *.so *.dylib
-rm -f cmake_install.cmake CMakeCache.txt Makefile
-rm -r CMakeFiles
-rm -r MyDynFramework.framework
+rm -r build
 
-cmake .
+mkdir build
+cd build
+
+cmake ..
 cmake --build .
