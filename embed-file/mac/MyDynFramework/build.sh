4bb54601d7e96cc843cc7d952681c13b9b5497e4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-07 14:54:02 +0200

    stop passing "-fPIC"

--- embed-file/mac/MyDynFramework/build.sh
+++ embed-file/mac/MyDynFramework/build.sh
@@ -5,7 +5,7 @@ echo ""
 echo Building MyDynFramework.dylib...
 
 # -fPIC set by default with dynamiclib
-g++ -fPIC -fvisibility=hidden -c MyDynFramework.cpp -o MyDynFramework.o
+g++ -fvisibility=hidden -c MyDynFramework.cpp -o MyDynFramework.o
 
 # NOTE: section names cannot be longer than 16 chars
 ld  MyDynFramework.o -r -sectcreate __TEXT embed_example embed_example.txt -o MyDynFramework.o
