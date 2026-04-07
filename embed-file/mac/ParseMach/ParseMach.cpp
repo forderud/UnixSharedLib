d922ef5b5ac9e8d3e7d7f27687cd3aaa1ae39160
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-07 15:07:36 +0200

    start refactoring

--- embed-file/mac/ParseMach/ParseMach.cpp
+++ embed-file/mac/ParseMach/ParseMach.cpp
@@ -16,6 +16,26 @@ void ParseSections(const char* file_ptr, const char* sect_ptr, uint32_t nsects) 
   }
 }
 
+void DumpSegments(const FileMap& file) {
+  mach_header_64 hdr{};
+  memcpy(&hdr, file.ptr(), sizeof(hdr));
+
+  const char* cmd_ptr = file.ptr() + sizeof(hdr);
+  for (uint32_t i = 0; i < hdr.ncmds; ++i) {
+    const auto* cmd = (const load_command*)cmd_ptr;
+    cmd_ptr += cmd->cmdsize;
+    //printf("Load command %u: cmd=0x%x, cmdsize=%u\n", i, cmd->cmd, cmd->cmdsize);
+
+    // only parse 64bit segment commands
+    if (cmd->cmd == LC_SEGMENT_64) {
+      const auto* seg = (const segment_command_64*)cmd;
+      printf("Segment command: segname=%s, vmaddr=0x%llx, vmsize=0x%llx, fileoff=0x%llx, filesize=0x%llx, nsects=%u\n",
+             seg->segname, seg->vmaddr, seg->vmsize, seg->fileoff, seg->filesize, seg->nsects);
+      ParseSections(file.ptr(), (const char*)seg + sizeof(segment_command_64), seg->nsects);
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     printf("usage: %s <macho-binary>\n", argv[0]);
@@ -42,20 +62,11 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  const char* cmd_ptr = file.ptr() + sizeof(hdr);
-  for (uint32_t i = 0; i < hdr.ncmds; ++i) {
-    const auto* cmd = (const load_command*)cmd_ptr;
-    cmd_ptr += cmd->cmdsize;
-    //printf("Load command %u: cmd=0x%x, cmdsize=%u\n", i, cmd->cmd, cmd->cmdsize);
-
-    // only parse 64bit segment commands
-    if (cmd->cmd == LC_SEGMENT_64) {
-      const auto* seg = (const segment_command_64*)cmd;
-      printf("Segment command: segname=%s, vmaddr=0x%llx, vmsize=0x%llx, fileoff=0x%llx, filesize=0x%llx, nsects=%u\n",
-             seg->segname, seg->vmaddr, seg->vmsize, seg->fileoff, seg->filesize, seg->nsects);
-      ParseSections(file.ptr(), (const char*)seg + sizeof(segment_command_64), seg->nsects);
-    }
-
+  if (argc == 2) {
+    DumpSegments(file);
+  } else {
+    const char* segment_name = argv[2];
+    printf("TODO: Seaching for segment %s in file...\n", segment_name);
   }
 
   printf("\n");
