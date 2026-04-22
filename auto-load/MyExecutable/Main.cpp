c2a5ab228b2a783eb50bcf7cf9b9e8f71a3c57df
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-22 22:47:39 +0200

    .

--- auto-load/MyExecutable/Main.cpp
+++ auto-load/MyExecutable/Main.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
 
+extern void mystaticlib_function();
+
+
 int main() {
     printf("Program startup.\n");
+    //mystaticlib_function();
     return 0;
 }
--- auto-load/MyStaticLib/MyStaticLib.cpp
+++ auto-load/MyStaticLib/MyStaticLib.cpp
@@ -1,5 +1,10 @@
 #include <cstdio>
 
+__attribute__((visibility("default")))
+void mystaticlib_function() {
+    printf("mystaticlib_function called.\n");
+}
+
 __attribute__((constructor))
 static void init_function () {
     printf("MyStaticLib loaded.\n");
