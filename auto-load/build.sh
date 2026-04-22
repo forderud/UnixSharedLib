b7712cb681358d5b53c16d706903a53a38fe68f4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-22 10:13:42 +0200

    .

--- auto-load/build.sh
+++ auto-load/build.sh
@@ -5,7 +5,7 @@ echo ""
 echo Building libmylib.so...
 REMOVE_UNREF="-ffunction-sections -fdata-sections -Wl,--gc-sections"
 g++ -fPIC -fvisibility=hidden $REMOVE_UNREF -c mylib.cpp -o mylib.o
-g++ -shared -o libmylib.so mylib.o embed_example.o
+g++ -shared -o libmylib.so mylib.o
 
 echo Building mainApp...
 g++ main.cpp -L. -lmylib -Wl,-rpath=. -o mainApp
