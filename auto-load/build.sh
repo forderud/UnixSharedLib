8b083e0996656de20e8bafc9dc533f45005e7be4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-22 11:10:10 +0200

    macOS baseline

--- auto-load/build.sh
+++ auto-load/build.sh
@@ -1,9 +1,21 @@
 echo Cleaning up...
 rm -f *.o *.so mainApp
 
+
+if [ "$(uname)" = "Darwin" ]; then
+    echo "Running on macOS (Darwin)."
+    REMOVE_UNREF=""
+    OPTIMIZE="-O2 -flto"
+    RPATH=""
+else
+    echo "Running on Linux."
+    REMOVE_UNREF="-ffunction-sections -fdata-sections -Wl,--gc-sections"
+    OPTIMIZE="-O2 -flto -fwhole-program"
+    RPATH="-Wl,-rpath=."
+fi
+
 echo ""
 echo Building libmylib.so...
-REMOVE_UNREF="-ffunction-sections -fdata-sections -Wl,--gc-sections"
 g++ -fPIC -fvisibility=hidden $REMOVE_UNREF -c mylib.cpp -o mylib.o
 g++ -shared -o libmylib.so mylib.o
 
@@ -11,8 +23,7 @@ echo Building mainApp...
 # Might also need to use the following parameters:
 # * Linux: -Wl,--no-as-needed
 # * Mac  : -Wl,-needed_library,/path/to/libmylib.dylib
-OPTIMIZE="-O2 -flto -fwhole-program"
-g++ main.cpp $OPTIMIZE $REMOVE_UNREF -L. -lmylib -Wl,-rpath=. -o mainApp
+g++ main.cpp $OPTIMIZE $REMOVE_UNREF -L. -lmylib $RPATH -o mainApp
 
 echo ""
 echo Running mainApp:
