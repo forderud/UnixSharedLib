bceb5730ee76b556f82c2c51159d25960185f135
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-24 13:47:38 +0200

    Delete redundant "mylib.cpp" logic that have now been replaced by MyDynFramework

--- embed-file/mac/MyDynFramework/build.sh
+++ embed-file/mac/MyDynFramework/build.sh
@@ -7,7 +7,13 @@ echo Building MyDynFramework.dylib...
 # -fPIC set by default with dynamiclib
 g++ -fvisibility=hidden -c MyDynFramework.cpp -o MyDynFramework.o
 
-# NOTE: section names cannot be longer than 16 chars
-ld  MyDynFramework.o -r -sectcreate __TEXT embed_example embed_example.txt -o MyDynFramework.o
+if [ "$(uname)" = "Darwin" ]; then
+  # NOTE: section names cannot be longer than 16 chars
+  ld  MyDynFramework.o -r -sectcreate __TEXT embed_example embed_example.txt -o MyDynFramework.o
+else
+  # convert embed_example.txt into an object file to binary embedding
+  # -z noexecstack
+  ld -r -b binary embed_example.txt -o embed_example.o
+fi
 
 g++ -dynamiclib -o MyDynFramework.dylib MyDynFramework.o
