6e6ede5fd7986bc9e8983d81cc6382df5d6ee703
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-24 12:20:04 +0200

    indent to ease readability

--- force-load/MySharedLib/MySharedLib.cpp
+++ force-load/MySharedLib/MySharedLib.cpp
@@ -2,10 +2,10 @@
 
 __attribute__((constructor))
 static void init_function () {
-    printf("MySharedLib loaded.\n");
+    printf("  MySharedLib loaded.\n");
 }
 
 __attribute__((visibility("default")))
 void mysharedlib_function() {
-    printf("mysharedlib_function called.\n");
+    printf("  mysharedlib_function called.\n");
 }
--- force-load/MyStaticLib/MyStaticLib.cpp
+++ force-load/MyStaticLib/MyStaticLib.cpp
@@ -2,10 +2,10 @@
 
 __attribute__((constructor))
 static void init_function () {
-    printf("MyStaticLib loaded.\n");
+    printf("  MyStaticLib loaded.\n");
 }
 
 __attribute__((visibility("default")))
 void mystaticlib_function() {
-    printf("mystaticlib_function called.\n");
+    printf("  mystaticlib_function called.\n");
 }
