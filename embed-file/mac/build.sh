2c52b61af9cad7f24d00e503b44aee61cf6a8537
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-24 13:37:11 +0200

    almost complete

--- embed-file/mac/build.sh
+++ embed-file/mac/build.sh
@@ -11,5 +11,10 @@ echo Running MyExecutable...
 MyExecutable/MyExecutable
 
 echo ""
-ParseMach/ParseMach MyDynFramework/MyDynFramework.framework/MyDynFramework embed_example
-ParseMach/ParseMach MyDynFramework/MyDynFramework.framework/MyDynFramework LibMetadata
+if [ "$(uname)" = "Darwin" ]; then
+  ParseMach/ParseMach MyDynFramework/MyDynFramework.framework/MyDynFramework embed_example
+  ParseMach/ParseMach MyDynFramework/MyDynFramework.framework/MyDynFramework LibMetadata
+else
+  ParseELF/ParseELF MyDynFramework/libMyDynFramework.so embed_example
+  ParseELF/ParseELF MyDynFramework/libMyDynFramework.so LibMetadata
+fi
