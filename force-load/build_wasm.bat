b7b58bc6e0672915a019d84877121de106e8bae1
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-28 16:09:30 +0200

    Switch to standard emscripten image.

--- force-load/build_wasm.bat
+++ force-load/build_wasm.bat
@@ -10,12 +10,12 @@ mkdir build
 
 :: Valid choices are "Debug", "RelWithDebInfo", "Release" (also default)
 set BUILD_TYPE=%1
-set DOCKER_IMAGE=forderud/qtwasm:latest
-set MOUNT_SOURCE=-v %cd%:/project/source
-set MOUNT_BUILD=-v %cd%\build:/project/build
+set DOCKER_IMAGE=emscripten/emsdk:4.0.7
+set MOUNT_SOURCE=-v %cd%:/src
+set MOUNT_BUILD=-v %cd%\build:/src/build
 
 :: Default build settings
-docker run --rm %MOUNT_SOURCE% %MOUNT_BUILD% %DOCKER_IMAGE% || exit /b 1
+docker run --rm %MOUNT_SOURCE% %MOUNT_BUILD% %DOCKER_IMAGE% sh -c "emcmake cmake -S . -B build; cmake --build build" || exit /b 1
 :: Don't expose sources through webserver
 set MOUNT_SOURCE=
 
@@ -25,7 +25,7 @@ start http://localhost:8080/build/MyExecutable/MyExecutable.html
 :: Serve the webpage from the same docker container,
 :: so that file paths match build-time paths.
 docker run --rm -p 8080:8080 ^
-    -v %cd%\WebServer.py:/project/WebServer.py ^
+    -v %cd%\WebServer.py:/src/WebServer.py ^
     %MOUNT_SOURCE% %MOUNT_BUILD% ^
-    -w /project %DOCKER_IMAGE% ^
+    -w /src %DOCKER_IMAGE% ^
     python3 WebServer.py 8080
