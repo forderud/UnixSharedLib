7a765300748169ac4bbc319f79ab9cec5f9e49e4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-30 21:12:41 +0200

    Fix failing macOS and Linux builds.

--- force-load/build.sh
+++ force-load/build.sh
@@ -3,6 +3,8 @@ set -e   # stop on first failure
 echo Cleaning up...
 rm -rf build install test
 
+CONFIG=""
+#CONFIG="--config Debug"
 
 # Open "Keychain Access". Select "login" keychain, filter by "Certificates" and look for a "Apple. Development: <your name>"  certificate. The 10-character development team code is found in the "Organizational Unit" attribute.
 #XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="<TODO>"
@@ -15,17 +17,17 @@ BUILD_PARAMS=""
 
 echo Building projects...
 cmake -S . -B build $PLATFORM
-cmake --build build --config Debug $BUILD_PARAMS
+cmake --build build $CONFIG $BUILD_PARAMS
 
 echo Installing projects...
-cmake --install build --config Debug --prefix install
+cmake --install build $CONFIG --prefix install
 
 echo Running MyExecutable...
 build/MyExecutable/MyExecutable
 
 echo Building independent test...
 cmake -DCMAKE_PREFIX_PATH="../install" -S IndependentTest -B test $PLATFORM
-cmake --build test
+cmake --build test $CONFIG
 
 echo Running independent test...
 test/IndependentTest
