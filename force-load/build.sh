d7a0b9a2cd962846432f0f6c436f32971d39848c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-30 09:52:49 +0200

    add commented-out support for iOS cross-compilation

--- force-load/build.sh
+++ force-load/build.sh
@@ -3,8 +3,11 @@ set -e   # stop on first failure
 echo Cleaning up...
 rm -rf build install test
 
+PLATFORM=""
+#PLATFORM="-GXcode -DCMAKE_SYSTEM_NAME=iOS"
+
 echo Building projects...
-cmake -S . -B build
+cmake -S . -B build $PLATFORM
 cmake --build build
 
 echo Installing projects...
@@ -14,7 +17,7 @@ echo Running MyExecutable...
 build/MyExecutable/MyExecutable
 
 echo Building independent test...
-cmake -DCMAKE_PREFIX_PATH="../install" -S IndependentTest -B test
+cmake -DCMAKE_PREFIX_PATH="../install" -S IndependentTest -B test $PLATFORM
 cmake --build test
 
 echo Running independent test...
