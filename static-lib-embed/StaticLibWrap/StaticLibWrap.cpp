eafd7b8e2b1b71141bb5243f5ce7f8b653f37ec3
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-30 21:51:40 +0200

    .

--- static-lib-embed/StaticLibWrap/StaticLibWrap.cpp
+++ static-lib-embed/StaticLibWrap/StaticLibWrap.cpp
@@ -1,8 +1,11 @@
 #include <cstdio>
+#include <MyStaticLib.hpp>
 
 // exported function
 extern "C" // avoid name mangling
 __attribute__((visibility("default")))
 void staticlibwrap_public () {
     printf("  staticlibwrap_public called.\n");
+
+    mystaticlib_public();
 }
