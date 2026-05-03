2c1f7612426479305af40fbd4b93c7eee819dca1
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-03 18:50:02 +0200

    use command-line argument to select platform

--- static-lib-embed/build.sh
+++ static-lib-embed/build.sh
@@ -1,19 +1,23 @@
 set -e   # stop on first failure
 
+ARG=$1
+
 echo Cleaning up...
 rm -rf build install test
 
-CONFIG=""
-#CONFIG="--config Debug"
-
-# Open "Keychain Access". Select "login" keychain, filter by "Certificates" and look for a "Apple. Development: <your name>"  certificate. The 10-character development team code is found in the "Organizational Unit" attribute.
-#XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="<TODO>"
-
-PLATFORM=""
-#PLATFORM="-GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$XCODE_ATTRIBUTE_DEVELOPMENT_TEAM -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH"
-
-BUILD_PARAMS=""
-#BUILD_PARAMS="-- -allowProvisioningUpdates"
+if [ "$ARG" == "iOS" ]; then
+    echo Building for iOS
+    # Open "Keychain Access". Select "login" keychain, filter by "Certificates" and look for a "Apple. Development: <your name>"  certificate. The 10-character development team code is found in the "Organizational Unit" attribute.
+    #XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="<TODO>"
+    CONFIG="--config Debug"
+    PLATFORM="-GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$XCODE_ATTRIBUTE_DEVELOPMENT_TEAM -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH"
+    BUILD_PARAMS="-- -allowProvisioningUpdates"
+else
+    echo Building for native platform
+    CONFIG=""
+    PLATFORM=""
+    BUILD_PARAMS=""
+fi
 
 echo Building projects...
 cmake -S . -B build $PLATFORM
@@ -22,12 +26,14 @@ cmake --build build $CONFIG $BUILD_PARAMS
 echo Installing projects...
 cmake --install build $CONFIG --prefix install
 
-echo Running MyExecutable...
-build/MyExecutable/MyExecutable
+if [ "$ARG" == "" ]; then
+    echo Running MyExecutable...
+    build/MyExecutable/MyExecutable
 
-echo Building independent test...
-cmake -DCMAKE_PREFIX_PATH="../install" -S IndependentTest -B test $PLATFORM
-cmake --build test $CONFIG $BUILD_PARAMS
+    echo Building independent test...
+    cmake -DCMAKE_PREFIX_PATH="../install" -S IndependentTest -B test $PLATFORM
+    cmake --build test $CONFIG $BUILD_PARAMS
 
-echo Running independent test...
-test/IndependentTest
+    echo Running independent test...
+    test/IndependentTest
+fi
