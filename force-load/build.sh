7bfd58f3fbec2d1e330adab61d10776fb9d6900e
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-03 18:37:11 +0200

    use command-line argument to select platform

--- force-load/build.sh
+++ force-load/build.sh
@@ -1,19 +1,23 @@
 set -e   # stop on first failure
 
+ARG=$1
+
 echo Cleaning up...
 rm -rf build install test
 
-CONFIG=""
-#CONFIG="--config Debug"
-
-# Open "Keychain Access". Select "login" keychain, filter by "Certificates" and look for a "Apple. Development: <your name>"  certificate. The 10-character development team code is found in the "Organizational Unit" attribute.
-#XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="<TODO>"
-
-PLATFORM=""
-#PLATFORM="-GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$XCODE_ATTRIBUTE_DEVELOPMENT_TEAM -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH"
-
-BUILD_PARAMS=""
-#BUILD_PARAMS="-- -allowProvisioningUpdates"
+if [ "$ARG" == "iOS" ]; then
+    echo Building for iOS platform
+    # Open "Keychain Access". Select "login" keychain, filter by "Certificates" and look for a "Apple. Development: <your name>"  certificate. The 10-character development team code is found in the "Organizational Unit" attribute.
+    #XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="<TODO>"
+    CONFIG="--config Debug"
+    PLATFORM="-GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$XCODE_ATTRIBUTE_DEVELOPMENT_TEAM -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH"
+    BUILD_PARAMS="-- -allowProvisioningUpdates"
+else
+    echo Building for native platform
+    CONFIG=""
+    PLATFORM=""
+    BUILD_PARAMS=""
+fi
 
 echo Building projects...
 cmake -S . -B build $PLATFORM
