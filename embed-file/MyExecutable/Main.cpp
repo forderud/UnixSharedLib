4b0c1f811e915b292dde1dc8d8afc463350eb3bd
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-03 21:05:43 +0200

    Start working on macOS bundle content iteration.

--- embed-file/MyExecutable/Main.cpp
+++ embed-file/MyExecutable/Main.cpp
@@ -1,8 +1,39 @@
+#ifdef __APPLE__
+    #include <CoreFoundation/CoreFoundation.h>
+#endif
+#include <iostream>
+#include <filesystem>
 #include <MySharedLib/MySharedLib.hpp>
+
+
+#ifdef __APPLE__
+static std::string get_bundle_resource_path() {
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef resourceURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[1024];
+    if (CFURLGetFileSystemRepresentation(resourceURL, true, (UInt8 *)path, 1024)) {
+        CFRelease(resourceURL);
+        return std::string(path);
+    }
+    CFRelease(resourceURL);
+    return "";
+}
+#endif
 
 
 int main() {
     // call function in static library
     print_embedded_file("embed_example");
+
+#ifdef __APPLE__
+    std::string path = get_bundle_resource_path();
+    printf("Listing bundle resources in %s:\n", path.c_str());
+    #if 0
+    for (const auto & entry : std::filesystem::directory_iterator(path)) {
+        std::cout << entry.path().filename() << std::endl;
+    }
+    #endif
+#endif
+
     return 0;
 }
