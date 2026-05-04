cc6eb472b755f6078cf66fb35822295049a01111
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-04 10:32:56 +0200

    iphonesimulator statup now works, but the app crashes.

--- embed-file/build.sh
+++ embed-file/build.sh
@@ -55,5 +55,5 @@ fi
 if [ "$ARG" = "iOS" ]; then
     echo Running MyExecutable on iOS Simulator...
     xcrun simctl install booted MyExecutable/Debug-iphonesimulator/MyExecutable.app
-    #xcrun simctl launch booted com.yourcompany.MyExecutable
+    xcrun simctl launch booted MyExecutable
 fi
