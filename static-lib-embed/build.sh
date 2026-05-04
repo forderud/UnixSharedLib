d0dd4bd07ad25457a1fb81e5d297fad494f38d93
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-04 09:37:26 +0200

    .

--- static-lib-embed/build.sh
+++ static-lib-embed/build.sh
@@ -14,6 +14,7 @@ if [ "$ARG" = "iOS" ]; then
     BUILD_PARAMS="-- -allowProvisioningUpdates"
 elif [ "$ARG" = "Android" ]; then
     echo Building for Android
+    #ANDROID_NDK=$HOME/Library/Android/sdk/ndk/<version>
     CONFIG=""
     PLATFORM="-DCMAKE_BUILD_TYPE=Debug -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=$ANDROID_NDK -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH"
     BUILD_PARAMS=""
