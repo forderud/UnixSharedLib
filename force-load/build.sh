c40c25194022792419c43887b259da446045bf60
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-04 20:21:24 +0200

    Remove instructions for how to extract the Xcode developer team ID from a keychain certificate.

--- force-load/build.sh
+++ force-load/build.sh
@@ -7,7 +7,6 @@ rm -rf build install test
 
 if [ "$ARG" = "iOS" ]; then
     echo Building for iOS
-    # Open "Keychain Access". Select "login" keychain, filter by "Certificates" and look for a "Apple. Development: <your name>"  certificate. The 10-character development team code is found in the "Organizational Unit" attribute.
     #XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="<TODO>"
     CONFIG="--config Debug"
     PLATFORM="-GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$XCODE_ATTRIBUTE_DEVELOPMENT_TEAM -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH"
