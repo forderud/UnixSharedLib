e3f28509c83dc69e5f293634f0b324f0e80f6af9
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-14 16:07:21 +0200

    printf wrapper for Android

--- wip/android-sample/MySharedLib.cpp
+++ wip/android-sample/MySharedLib.cpp
@@ -1,8 +1,13 @@
-#include <android/log.h>
+#ifdef __ANDROID__
+  #include <android/log.h>
+  #define printf(...) __android_log_print(ANDROID_LOG_DEBUG, "MySharedLib", __VA_ARGS__)
+#else
+    #include <stdio.h>
+#endif
 #include "MySharedLib.hpp"
 
 
 __attribute__ ((visibility ("default")))
 void MySharedLib_function () {
-    __android_log_print(ANDROID_LOG_INFO, "MySharedLib", "sharedlib_function called.");
+    printf("sharedlib_function called.");
 }
