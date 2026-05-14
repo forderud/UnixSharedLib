ca5a81eedc22eda404475f1bf063697e03ac9c42
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-14 17:15:06 +0200

    remove uneccesary Android entry point

--- embed-file/MyExecutable/Main.cpp
+++ embed-file/MyExecutable/Main.cpp
@@ -47,13 +47,6 @@ static void openAsset(JNIEnv* env, jobject assetManager, const char* filename) {
 
     // Process asset data here
 }
-
-/** Android entry point */
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_myapp_MainActivity_stringFromJNI(JNIEnv* env, jobject /*this*/) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
 #endif
 
 
