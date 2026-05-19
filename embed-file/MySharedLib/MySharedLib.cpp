a34dbe5c3b33b0a60b9cdcc3115e58c608d64d68
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-05-19 20:46:10 +0200

    more log redirect unification

--- embed-file/MySharedLib/MySharedLib.cpp
+++ embed-file/MySharedLib/MySharedLib.cpp
@@ -1,11 +1,6 @@
 #include <string>
 #include <dlfcn.h>
-#ifdef __ANDROID__
-  #include <android/log.h>
-  #define printf(...) __android_log_print(ANDROID_LOG_DEBUG, "MySharedLib", __VA_ARGS__)
-#else
-    #include <stdio.h>
-#endif
+#include "../LogRedirect.hpp"
 #if defined(__APPLE__)
   // "mach-o" header folder path: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/mach-o/
   #include <mach-o/getsect.h> // for getsectiondata
